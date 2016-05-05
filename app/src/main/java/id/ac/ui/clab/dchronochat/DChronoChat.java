b034d6b6971a2c4e180f44c1cdf1264f866842e0
yudiandreanp <yudiandreanp@gmail.com>
2016-05-05 18:08:56 +0700

    Initial commit

---
+++ app/src/main/java/id/ac/ui/clab/dchronochat/DChronoChat.java
@@ -0,0 +1,21 @@
+package id.ac.ui.clab.dchronochat;
+
+
+import net.named_data.jndn.sync.ChronoSync2013;
+
+/**
+ * Created by yudiandreanp on 03/05/16.
+ */
+
+
+public class DChronoChat {
+
+    private String screenName;
+
+
+    public DChronoChat(String screenName)
+    {
+
+    }
+
+}
